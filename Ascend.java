3596b6711cff383c253549529e805e2760fa28c8
Tamar-Levy03 <Tamar.levy03@post.runi.ac.il>
2024-11-07 16:13:50 +0200

    check

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,23 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		String lim = args[0];
+		int limint = Integer.parseInt(lim);
+		double a = Math.random()*limint;
+		double b = Math.random()*limint;
+		double c = Math.random()*limint;
+		int inta= (int) a;
+		int intb = (int) b;
+		int intc = (int) c;
+		System.out.println(inta + " " + intb + " " + intc);
+		int min = Math.min(inta, intb); 
+		int min2 = Math.min(min, intc);
+		int max = Math.max(inta, intb);
+		int max2 = Math.max(max,intc);
+		int mid = Math.min(max, Math.max(intb, intc));
+		int mid2 = Math.min(mid, Math.max(inta, intc));
+		System.out.println(min2 + " " + mid2+ " " + max2);
+
+
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+		String num = args[0];
+		int numint = Integer.parseInt(num);
+		int hundreds = numint/100;
+		int tens = (numint/10) % 10;
+		int ones = (numint % 10);
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and  " + ones + " ones." );
 	}
 }
